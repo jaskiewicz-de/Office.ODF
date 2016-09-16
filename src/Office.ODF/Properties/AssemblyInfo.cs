0175da5834ee978bf1ec3ed047a27ca6d74250e9
Lukasz Jaskiewicz <dev@jaskiewicz.de>
2016-09-16 21:14:19 +0200

    Office.ODF project added

---
+++ src/Office.ODF/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿[assembly: System.Reflection.AssemblyTitleAttribute("Office.ODF Library")]
+[assembly: System.Reflection.AssemblyDescriptionAttribute("Office.ODF Library")]
+[assembly: System.Reflection.AssemblyCopyrightAttribute("Copyright (c) 2016 Lukasz Jaskiewicz. All Rights Reserved.")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("0.0.0.0")]
+[assembly: System.Reflection.AssemblyVersionAttribute("0.0.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("0.0.0")]
+[assembly: System.Runtime.Versioning.TargetFrameworkAttribute(".NETStandard,Version=v1.6")]
